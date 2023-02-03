9a871957babf10800d75c33e9c19383955ceef43
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-03 11:03:46 +0100

    removed excess string literal in part 2

--- 00._Miscellaneous/1._Intro_Exercises/1.Basics.js
+++ 00._Miscellaneous/1._Intro_Exercises/1.Basics.js
@@ -19,6 +19,6 @@ const number = 1;
 // Add the year plus the number
 // The result should be 2023
 // You cannot touch line 1 or 2
-console.log(`${parseInt(year) + number}`);
+console.log(parseInt(year) + number);
 
 // --------------------------------------
