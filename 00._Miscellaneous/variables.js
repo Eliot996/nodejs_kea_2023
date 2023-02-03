a9e49fd824d84357c7c43d1c044f47ade75d2983
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-03 11:00:12 +0100

    reset my variables, accidentally overwrote from teachers version

--- 00._Miscellaneous/variables.js
+++ 00._Miscellaneous/variables.js
@@ -1,27 +1,21 @@
-// Const Rule 1: Have to assign a value when declaring.  
-// const me;
+// You have to assign to a value when declaring a const
+// const me
 
-const me = {
-    /* key: value 
-    === key-value pairs
-    */
-   name: "Hans"
-};
-const hobbies = ["Football", "Swimming"];
-hobbies.push("Cooking");
-me.hobbies = hobbies;
+const me  = {
+    // key: value -> key-value pairs
+    name: "hans"
+}
 
+// Cannot reassign to const
+// me = {} 
 
-// Const Rule 2: Cannot reassign to constant
-// me = {};
-me.name = "Johannes";
+// But it is not immutable
+me.name = "Cam"
 
-
-
-console.log(me);
+console.log(me)
 
 
 const hobbyOne = "Football skill level: '6'";
 const hobbyTwo = 'Swimming skill level: "3"';
-const hobbyThree = `Cooking skill level: "'${2 + 2}"'`;
-console.log(hobbyThree);
+const hobbtThree = `Cooking skill level: ${2 + 2}`; // string interpolation - string template literal, can use js inside of a string
+
