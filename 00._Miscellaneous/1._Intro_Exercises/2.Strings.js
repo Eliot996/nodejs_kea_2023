6875168a5f9d6cb3d0d75bbe953996bf76b0d3e6
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-03 13:13:20 +0100

    finished exercises  3 - 7

---
+++ 00._Miscellaneous/1._Intro_Exercises/2.Strings.js
@@ -0,0 +1,59 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+let result = Number(numberOne) + Number(numberTwo)
+console.log(result);
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+// --------------------------------------
+result = Number(anotherNumberOne) + Number(anotherNumberTwo)
+console.log(result.toFixed(2));
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+result = (one + two + three) / 3
+
+console.log(result.toFixed(5));
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+
+result = letters[2];
+
+console.log(result);
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript 18
+result = fact.slice(0,17) + fact.charAt(17).toUpperCase() + fact.slice(18)
+console.log(result);
+
+// --------------------------------------
+
+
+
