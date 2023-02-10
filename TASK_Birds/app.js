24e0b80bd0caae926ac508cb5cd67ef549324481
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-10 13:06:52 +0100

    added TASK_birds with initial solution

---
+++ TASK_Birds/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const birds = [
+    {name: "sparrow"}, 
+    {name: "hawk"},  
+];
+
+app.get('/birds', (req, res) => {
+    res.send({data: birds})
+});
+
+app.get('/birds/:id', (req, res) => {
+    res.send({data: birds[req.params.id]})
+});
+
+app.listen(8080);
