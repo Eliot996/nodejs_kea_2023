f2b5470dd26452d2df60193991298971981ec9be
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-17 09:50:19 +0100

    added filter on even aged rocks

--- 00._Miscellaneous/loops.js
+++ 00._Miscellaneous/loops.js
@@ -1,7 +1,7 @@
 const rocks = [
     { name: "Pet rock", age: 2},
-    { name: "Led Zeppelin", age: 50},
-    { name: "Dwayne Johnson", age: 47},
+    { name: "Led Zeppelin", age: 55},
+    { name: "Dwayne Johnson", age: 50},
     { name: "Neptune", age: 100_000_000_000},
 ]
 
@@ -13,3 +13,11 @@ const rocksAgedOneYear = rocks.map(rock => {
 
 console.log(rocks);
 console.log(rocksAgedOneYear);
+
+console.log("");
+console.log("");
+console.log("Even rocks: ");
+
+const evenAgedRocks = rocks.filter(( rock => rock.age % 2 === 0));
+
+console.log(evenAgedRocks);
