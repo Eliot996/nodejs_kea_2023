7143778cae041cf57774980099a5daac7d12a400
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-17 10:40:09 +0100

    basic setup

--- 03._Plain_HTML_Server/app.js
+++ 03._Plain_HTML_Server/app.js
@@ -1,2 +1,16 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send()
+});
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log('Server is running on port', PORT);
+});
