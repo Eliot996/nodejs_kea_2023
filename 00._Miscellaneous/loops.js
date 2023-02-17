708ef9b523a120a7097150899f3fba10f3aa7984
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-17 10:22:08 +0100

    linted

--- 00._Miscellaneous/loops.js
+++ 00._Miscellaneous/loops.js
@@ -1,12 +1,12 @@
 const rocks = [
-    { name: "Pet rock", age: 2},
-    { name: "Led Zeppelin", age: 55},
-    { name: "Dwayne Johnson", age: 50},
-    { name: "Neptune", age: 100_000_000_000},
+    { name: "Pet rock", age: 2 },
+    { name: "Led Zeppelin", age: 55 },
+    { name: "Dwayne Johnson", age: 50 },
+    { name: "Neptune", age: 100_000_000_000 },
 ]
 
 console.log("Aged rocks: ");
-const rocksAgedOneYear = rocks.map(rock => ({...rock, age: rock.age+1}))
+const rocksAgedOneYear = rocks.map(rock => ({ ...rock, age: rock.age + 1 }));
 
 console.log(rocks);
 console.log(rocksAgedOneYear);
@@ -15,8 +15,8 @@ console.log();
 console.log("Even rocks: ");
 
 const evenAgedRocks = rocks
-    .filter(( rock => rock.age % 2 === 0))
-    .map(rock => ({...rock}));
+    .filter(rock => rock.age % 2 === 0)
+    .map(rock => ({ ...rock }));
 
 console.log(evenAgedRocks);
 console.log(rocks);
