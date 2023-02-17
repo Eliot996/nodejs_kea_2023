c76a9da3d8b251179437ba0ebec43bd0b576a23a
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-17 09:33:01 +0100

    init with solution to task_birds

---
+++ 02._Birds_API/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const birds = [
+    {id: 1, name: "Violet-backed starling", maleRating: 10, femaleRating: 2}
+];
+
+app.get("/birds", (req, res) => {
+    res.send({date: birds});
+});
+
+app.get("/birds/:id", (req, res) => {
+    const foundBird = birds.find((bird => bird.id === Number(req.params.id)));
+
+    res.send({date: foundBird});
+});
+
+app.listen(8080, () => {
+    console.log("Server is running on port", 8080);
+});
