9bcf10c46192a6855f4f94c1b822c2d5a520e8f5
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-02-22 20:22:00 +0100

    added post

--- 02._Birds_API/app.js
+++ 02._Birds_API/app.js
@@ -1,20 +1,39 @@
 const express = require("express");
+
 const app = express();
 
 app.use(express.json());
 
 const birds = [
-    {id: 1, name: "Violet-backed starling", maleRating: 10, femaleRating: 2}
+    {
+        id: 1,
+        name: "Violet-backed starling",
+        maleRating: 10,
+        femaleRating: 2,
+    },
 ];
 
 app.get("/birds", (req, res) => {
-    res.send({date: birds});
+    res.send({ date: birds });
 });
 
 app.get("/birds/:id", (req, res) => {
-    const foundBird = birds.find((bird => bird.id === Number(req.params.id)));
+    const foundBird = birds.find(((bird) => bird.id === Number(req.params.id)));
 
-    res.send({date: foundBird});
+    res.send({ date: foundBird });
+});
+
+app.post("/birds", (req, res) => {
+    const maxId = birds.reduce(
+        (accumulator, bird) => ((bird.id > accumulator) ? bird.id : accumulator),
+        1,
+    );
+
+    req.body.id = maxId + 1;
+
+    birds.push(req.body);
+
+    res.send({ message: birds[birds.length - 1] });
 });
 
 app.listen(8080, () => {
