a2b1297a530e105441088f10ce63ebc1bf9b9f9c
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-03-02 18:56:38 +0100

    added date calculation to page

--- TASK_Time/public/frontpage/frontpage.js
+++ TASK_Time/public/frontpage/frontpage.js
@@ -1,3 +1,27 @@
+const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+
 function calculateBirthdays() {
-    console.log("hit");
+    const birthday = document.getElementById("birthday").valueAsDate;
+    
+    const formatter = new Intl.DateTimeFormat("en-US", options);
+    const list = document.getElementById("birthdayslist");
+
+    for (let i = 1; i <= 100; i++) {
+        const date = birthday.addYear(i);
+        const element = document.createElement("li");
+        element.innerHTML = formatter.format(date);
+        list.appendChild(element);
+    }
+}
+
+Date.prototype.addYear = function(years) {
+    const date = new Date(this.valueOf());
+    date.setFullYear(date.getFullYear() + years);
+    return date;
 }
