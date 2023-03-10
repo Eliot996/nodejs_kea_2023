facbb50a82dd29c286de838132104d77ec1664de
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-03-10 09:44:51 +0100

    init

---
+++ 04.Upper/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+const app = express();
+
+import path from "path";
+
+app.use(express.static("public"));
+
+// import jokes from "./util/jokes.js";
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/pages/frontpage/frontpage.html"));
+});
+
+app.get("/IRLQuests", (req, res) => {
+    res.sendFile(path.resolve("public/pages/IRLQuests/IRLQuests.html"));
+});
+
+app.get("/jokes", () => {
+    res.sendFile(path.resolve("public/pages/jokes/jokes.html"))
+});
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server running on port", PORT);
+});
