3afba03c564ab3bb63dc7c13b5381e74fd4bf69a
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-03-17 10:39:49 +0100

    minor change

--- 04.Upper/util/templateEngine.js
+++ 04.Upper/util/templateEngine.js
@@ -3,7 +3,7 @@ import fs from "fs";
 export default function renderPage(pagePath, config = {}) {
     const navbar = fs.readFileSync("./public/components/navbar/navbar.html").toString()
         .replace("$TAB_TITLE", config.tabTitle || "Upper")
-        .replace("$CSS_LINK",  config.cssLink || "");
+        .replace("$CSS_LINK",  config.cssLink  || "");
 
     const footer = fs.readFileSync("./public/components/footer/footer.html").toString()
         .replace("$FOOTER_YEAR", `© ${new Date().getFullYear()}`);
