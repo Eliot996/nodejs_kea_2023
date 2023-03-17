8f5441efb29cd9baaa7eecda8a2f22f9c95bd27c
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-03-17 09:23:34 +0100

    now using components and SSR

--- 04.Upper/app.js
+++ 04.Upper/app.js
@@ -6,18 +6,33 @@ import path from "path";
 import jokes from "./util/jokes.js";
 
 app.use(express.static("public"));
-//console.log(await jokes.getJoke());
+
+import fs from "fs";
+// Components
+const navbar = fs.readFileSync("./public/components/navbar/navbar.html").toString();
+const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
+
+// Pages
+const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString();
+const IRLQuests = fs.readFileSync("./public/pages/IRLQuests/IRLQuests.html").toString();
+const jokesContent     = fs.readFileSync("./public/pages/jokes/jokes.html").toString();
+
+// constructed pages
+const frontpagePage = navbar + frontpage + footer;
+const IRLQuestsPage = navbar + IRLQuests + footer;
+const jokesPage = navbar + jokesContent + footer;
+
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public/pages/frontpage/frontpage.html"));
+    res.send(frontpagePage);
 });
 
 app.get("/IRLQuests", (req, res) => {
-    res.sendFile(path.resolve("public/pages/IRLQuests/IRLQuests.html"));
+    res.send(IRLQuestsPage);
 });
 
 app.get("/jokes", (req, res) => {
-    res.sendFile(path.resolve("public/pages/jokes/jokes.html"))
+    res.send(jokesPage)
 });
 
 const PORT = 8080;
