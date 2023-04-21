7bda07b74705f8b3b0af559c7693283fd6c2580e
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-04-21 10:18:06 +0200

    added reset script

--- 09._Solar_System/server/databases/connection.js
+++ 09._Solar_System/server/databases/connection.js
@@ -2,7 +2,6 @@ import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 
 
-
 const connection = await open({
     filename: './solar_system.db',
     driver: sqlite3.Database
--- 09._Solar_System/server/databases/createDatabase.js
+++ 09._Solar_System/server/databases/createDatabase.js
@@ -1,7 +1,15 @@
 import db from "./connection.js"
 
-// Table creation
-db.exec(`CREATE TABLE IF NOT EXISTS plantes (
+const isDeleteMode = process.argv.findIndex((arg) => arg ==="delete_mode") === -1 ? false : true;
+console.log(isDeleteMode);
+
+if (isDeleteMode) {
+   db.exec("DROP TABLE IF EXISTS planets") 
+   db.exec("DROP TABLE IF EXISTS people") 
+}
+
+// Table creation (DDL)
+db.exec(`CREATE TABLE IF NOT EXISTS planets (
     id   INTEGER PRIMARY KEY AUTOINCREMENT, 
     name TEXT, 
     size FLOAT, 
@@ -12,3 +20,15 @@ db.exec(`CREATE TABLE IF NOT EXISTS people (
     id   INTEGER PRIMARY KEY AUTOINCREMENT, 
     name TEXT
     )`);
+
+// Seeding (DML)
+if (isDeleteMode) {
+    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Mercury', False)`);
+    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Venus', False)`);
+    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Earth', True)`);
+    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Mars', False)`);
+    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Jupiter', False)`);
+    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Saturn', False)`);
+    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Uranus', False)`);
+    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Neptune', False)`);
+}
