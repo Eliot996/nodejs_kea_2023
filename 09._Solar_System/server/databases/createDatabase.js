ebebb896a08849b85f6267c010c748ffe8de4598
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-04-21 09:57:08 +0200

    added people table

--- 09._Solar_System/server/databases/createDatabase.js
+++ 09._Solar_System/server/databases/createDatabase.js
@@ -1,8 +1,14 @@
 import db from "./connection.js"
 
-db.exec(`CREATE TABLE Planets (
+// Table creation
+db.exec(`CREATE TABLE IF NOT EXISTS plantes (
     id   INTEGER PRIMARY KEY AUTOINCREMENT, 
     name TEXT, 
     size FLOAT, 
     is_habitable BOOLEAN
+    )`);
+
+db.exec(`CREATE TABLE IF NOT EXISTS people (
+    id   INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT
     )`);
