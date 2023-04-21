d3059c41c0b1d5e10c4657f7f30b0d4cfdc8b13c
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-04-21 09:17:38 +0200

    init 09

---
+++ 09._Solar_System/server/app.js
@@ -0,0 +1,7 @@
+import express  from "express";
+const app = express();
+
+
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => console.log("Server is running on", server.address().port));
