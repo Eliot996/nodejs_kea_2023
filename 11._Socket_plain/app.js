f360fe9be2388d2b1abd71ae371feae0e61088fd
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-05-12 09:51:50 +0200

    sending and recieving color

--- 11._Socket_plain/app.js
+++ 11._Socket_plain/app.js
@@ -12,8 +12,17 @@ const io = new Server(server);
 io.on("connection", (socket) => {
     console.log("A client has connected", socket.id);
 
-    socket.on("ready event", (data) => {
-        console.log("From the ready event: ", data.data);
+    socket.on("color choice", ({ data }) => {
+        console.log(socket.id, "chose this color: ", data);
+
+        // sends only to the socket
+        // socket.emit("a color was chosen", { data: data} );
+        
+        // broadcast to all but the socket itself
+        // socket.broadcast.emit("a color was chosen", { data: data} );
+        
+        // sending to all in namespace
+        io.emit("a color was chosen", { data: data})
     });
 });
 
