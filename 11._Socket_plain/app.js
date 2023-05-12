0b20f18620506ead2a2498ebd935a034867544dc
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-05-12 09:02:20 +0200

    init

---
+++ 11._Socket_plain/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+app.use(express.static("public"));
+
+import http from "http";
+const server = http.createServer(app);
+
+import { Server } from "socket.io";
+const io = new Server(server);
+
+
+io.on("connection", (socket) => {
+    console.log("A client has connected", socket.id);
+});
+
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log("server is running on ", PORT))
+
